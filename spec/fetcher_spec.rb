bc7da7a8e43d98f6c8f967eb1bd710c093ce0bcd
Trevor Parker <trevor@trevorparker.com>
2014-03-06 21:40:36 -0500

    Mock web requests, responses, and their headers
    This adds mock web requests along with slightly more rigorous
    web cache tests. The Fetcher spec can probably be broken down
    into more concise chunks, but this works for now.

--- spec/fetcher_spec.rb
+++ spec/fetcher_spec.rb
@@ -1,30 +1,32 @@
 require 'spec_helper'
 
 describe(RSSCache::Fetcher) do
-  let(:last_modified_feed_url) { 'http://test.trevorparker.com/rss.xml' }
-  let(:etag_feed_url) { 'https://github.com/trevorparker.atom' }
+  let(:last_modified_feed_url) { 'http://example.com/mock-rss-304.xml' }
+  let(:etag_feed_url) { 'http://example.com/mock-atom-304.xml' }
 
   it 'can stash Last-Modified header' do
-    f = RSSCache::Fetcher.new url: last_modified_feed_url
+    f = RSSCache::Fetcher.new url: @rss_feed_url
     f.fetch
     expect(f.last_modified).to be_a String
   end
 
   it 'can stash etag' do
-    f = RSSCache::Fetcher.new url: etag_feed_url
+    f = RSSCache::Fetcher.new url: @atom_feed_url
     f.fetch
     expect(f.etag).to be_a String
   end
 
-  it 'can pass If-Modified-Since and honor 304 Not Modified response' do
+  it 'can honor a 304 Not Modified response' do
     f = RSSCache::Fetcher.new url: last_modified_feed_url
     2.times { f.fetch }
+    expect(FakeWeb.last_request['If-Modified-Since']).to eq @mock_last_modified
     expect(f.status).to eq 304
   end
 
-  it 'can pass If-None-Match and honor 304 Not Modified response' do
+  it 'can honor a 304 Not Modified response' do
     f = RSSCache::Fetcher.new url: etag_feed_url
     2.times { f.fetch }
+    expect(FakeWeb.last_request['If-None-Match']).to eq @mock_etag
     expect(f.status).to eq 304
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'fakeweb'
 require 'rsscache'
 
 RSpec.configure do |config|
@@ -6,9 +7,59 @@ RSpec.configure do |config|
   config.filter_run :focus
 
   config.before(:all) do
-    @rss_feed_url = 'http://test.trevorparker.com/rss.xml'
-    @atom_feed_url = 'https://github.com/trevorparker.atom'
-    @xml_feed_url = 'http://test.trevorparker.com/sitemap.xml'
+    @mock_last_modified = 'Sat, 15 Feb 2014 19:31:13 GMT'
+    @mock_etag = '49e2e8b303e355ff5302feb5ffe2baa2'
+
+    mock_rss_feed = File.read(
+      File.join(File.dirname(__FILE__), 'fixtures', 'rss.xml')
+    )
+    mock_atom_feed = File.read(
+      File.join(File.dirname(__FILE__), 'fixtures', 'atom.xml')
+    )
+    mock_sitemap = File.read(
+      File.join(File.dirname(__FILE__), 'fixtures', 'sitemap.xml')
+    )
+
+    FakeWeb.register_uri(
+      :get, 'http://example.com/mock-rss.xml',
+      body: mock_rss_feed,
+      last_modified: @mock_last_modified
+    )
+    FakeWeb.register_uri(
+      :get, 'http://example.com/mock-atom.xml',
+      body: mock_atom_feed,
+      'ETag' => @mock_etag
+    )
+    FakeWeb.register_uri(
+      :get, 'http://example.com/sitemap.xml',
+      body: mock_sitemap
+    )
+
+    FakeWeb.register_uri(
+      :get, 'http://example.com/mock-rss-304.xml',
+      [
+        {
+          body: mock_rss_feed,
+          last_modified: @mock_last_modified
+        },
+        { status: ['304', 'Not Modified'] }
+      ]
+    )
+
+    FakeWeb.register_uri(
+      :get, 'http://example.com/mock-atom-304.xml',
+      [
+        {
+          body: mock_atom_feed,
+          'ETag' => @mock_etag
+        },
+        { status: ['304', 'Not Modified'] }
+      ]
+    )
+    @rss_feed_url  = 'http://example.com/mock-rss.xml'
+    @atom_feed_url = 'http://example.com/mock-atom.xml'
+    @xml_feed_url  = 'http://example.com/sitemap.xml'
+
     @rss_feed = RSSCache::Feed.new url: @rss_feed_url
     @atom_feed = RSSCache::Feed.new url: @atom_feed_url
   end
